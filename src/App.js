7fbf015fbf2c170c87b65e788fe2a0b9151ee079
aradgonen <gonena.arad@gmail.com>
2020-04-06 19:13:37 +0300

    First Version

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Racktable from './rack-table/racktable';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <Racktable></Racktable>
+  );
+}
+export default App;
