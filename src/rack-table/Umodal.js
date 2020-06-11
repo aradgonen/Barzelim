20fca513276f490b0148b1bcef126ae34b3c26ba
Arad Gonen <gonena.arad@gmail.com>
2020-06-11 09:33:22 +0300

    adeed nested modal

--- src/rack-table/Umodal.js
+++ src/rack-table/Umodal.js
@@ -1,12 +1,13 @@
 import Modal from "react-bootstrap/Modal";
-import {Button} from "react-bootstrap"
+import {Button, Dropdown} from "react-bootstrap"
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 class UModal extends React.Component {
 
     render() {
         return (
-            <Modal show={this.props.show}  animation={true}>
+            <Modal show={this.props.show}  animation={true} aria-labelledby="contained-modal-title-vcenter"
+            centered>
             <Modal.Header>
               <Modal.Title>More Data</Modal.Title>
             </Modal.Header>
@@ -15,6 +16,17 @@ class UModal extends React.Component {
               <Button variant="secondary" onClick={this.props.hide}>
                 Close
               </Button>
+              <Dropdown>
+              <Dropdown.Toggle variant="success" id="dropdown-basic">
+                Dropdown Button
+              </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+    <Dropdown.Item href="#/action-1">Open SSH Connection</Dropdown.Item>
+    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+  </Dropdown.Menu>
+</Dropdown>
             </Modal.Footer>
           </Modal>
         );
